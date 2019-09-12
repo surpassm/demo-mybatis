a57c53354e407bf9ccc1e18416f6a80b450a56ba
surpassm <lustres@sina.com>
2019-09-12 11:55:50 +0800

    修正日志BUG，添加全局异常处理

--- src/main/java/com/liaoin/demo/security/service/impl/RbacServiceImpl.java
+++ src/main/java/com/liaoin/demo/security/service/impl/RbacServiceImpl.java
@@ -1,5 +1,6 @@
 package com.liaoin.demo.security.service.impl;
 
+import com.liaoin.demo.entity.user.UserInfo;
 import com.liaoin.demo.security.service.RbacService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
@@ -21,6 +22,26 @@ public class RbacServiceImpl implements RbacService {
 	private AntPathMatcher antPathMatcher = new AntPathMatcher();
 	@Override
 	public boolean hasPermission(HttpServletRequest request, Authentication authentication) {
+		boolean flag = false;
+		Object principal = authentication.getPrincipal();
+		if (principal instanceof UserInfo){
+			//当前用戶
+			String username = ((UserInfo) principal).getUsername();
+//			Optional<UserInfo> userInfo = userInfoMapper.findByUsername(username);
+//			//当前用户所具备的权限
+//			List<UserMenu> menuList = userInfoMapper.findByUserRoles(userInfo.get().getUserRoles());
+//			//當前用戶所擁有權限的所有URL进行遍历查看用户是否具备权限
+//			String requestURI = request.getRequestURI();
+//			if (!requestURI.equals("/favicon.ico")){
+//				log.info("请求验证的URL"+requestURI);
+//				for (UserMenu menu : menuList){
+//					if (menu.getMenuUrl() != null && antPathMatcher.match(menu.getMenuUrl(),requestURI)){
+//						flag = true;
+//						break;
+//					}
+//				}
+//			}
+		}
 		return true;
 	}
 }
