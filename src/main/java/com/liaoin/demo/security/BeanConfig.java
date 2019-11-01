b95b6926a97f2db7aff9fdddc31b2feb54f7a625
lustres@sina.com <lustres@sina.com>
2019-11-01 15:29:45 +0800

    修正

--- src/main/java/com/liaoin/demo/security/BeanConfig.java
+++ src/main/java/com/liaoin/demo/security/BeanConfig.java
@@ -3,6 +3,7 @@ package com.liaoin.demo.security;
 import com.github.surpassm.security.exception.SurpassmAuthenticationException;
 import com.liaoin.demo.entity.user.UserInfo;
 import com.liaoin.demo.mapper.user.UserInfoMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
@@ -17,6 +18,7 @@ import java.util.Map;
  * Version 1.0
  * Description
  */
+@Slf4j
 @Configuration
 public class BeanConfig {
 	@Resource
@@ -25,19 +27,26 @@ public class BeanConfig {
 	private TokenStore tokenStore;
 
 	public UserInfo getAccessToken(String accessToken){
-		OAuth2Authentication oAuth2Authentication = tokenStore.readAuthentication(accessToken);
-		if (oAuth2Authentication == null){
-			throw new SurpassmAuthenticationException("token已失效");
-		}
-		Object principal = oAuth2Authentication.getPrincipal();
-		if(principal instanceof UserInfo){
-			return (UserInfo) principal;
-		}else if (principal instanceof String){
-			UserInfo userInfo = userInfoMapper.selectOne(UserInfo.builder().username(principal.toString()).build());
-			if (userInfo != null) {
-				return userInfo;
+		try {
+			OAuth2Authentication oAuth2Authentication = tokenStore.readAuthentication(accessToken);
+			if (oAuth2Authentication == null){
+				log.info("token已失效");
+				throw new SurpassmAuthenticationException("token已失效");
 			}
+			Object principal = oAuth2Authentication.getPrincipal();
+			if(principal instanceof UserInfo){
+				return (UserInfo) principal;
+			}else if (principal instanceof String){
+				UserInfo userInfo = userInfoMapper.selectOne(UserInfo.builder().username(principal.toString()).build());
+				if (userInfo != null) {
+					return userInfo;
+				}
+			}
+		}catch (Exception e){
+			log.info("tokens必须是JWT类型");
+			throw new SurpassmAuthenticationException("tokens必须是JWT类型");
 		}
+		log.info("请登陆");
 		throw new SurpassmAuthenticationException("请登陆");
 	}
 }
